3aa1ebab4e8e527ccc60f87953bb7876623163ec
eliezerrb <eliezerrezende@outlook.com>
2022-07-25 22:04:25 -0300

    Database seeding

--- backend/src/main/java/com/eliezerrb/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/eliezerrb/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eliezerrb.dscatalog.entities.Category;
 import com.eliezerrb.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
+	//@Transactional - Framework trata como uma transação do banco de dados ou faz tudo ou não faz nada
+	//readOnly = true - evita lock no DB só para ler dados
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
