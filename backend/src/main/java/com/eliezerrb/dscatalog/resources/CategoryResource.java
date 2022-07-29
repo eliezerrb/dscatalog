ba2a889acd43a4238d49cbfea6283d76607b9a90
eliezerrb <eliezerrezende@outlook.com>
2022-07-28 21:11:55 -0300

    POST new Category

--- backend/src/main/java/com/eliezerrb/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/eliezerrb/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.eliezerrb.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eliezerrb.dscatalog.dto.CategoryDTO;
 import com.eliezerrb.dscatalog.services.CategoryService;
@@ -29,9 +33,22 @@ public class CategoryResource {
 	// @PathVariable anotação para configurar o websevice para receber a variavel passada na rota (value = "/{id}")
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO>  findById(@PathVariable Long id){
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 		
 	}
+	
+	// @RequestBody - endpoint recoenher que é um objeto
+
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		
+		// Macete para inserir no cabeçalho da resposta o location
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
 }
