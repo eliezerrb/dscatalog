1a2983b9ac7efdbe711e6666cb1060c1c3fdd4db
eliezerrb <eliezerrezende@outlook.com>
2022-07-29 21:07:56 -0300

    DELETE Category

--- backend/src/main/java/com/eliezerrb/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/eliezerrb/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.eliezerrb.dscatalog.services.exceptions.DatabaseException;
 import com.eliezerrb.dscatalog.services.exceptions.ResourceNotFoundException;
 
 // @ControllerAdvice - Anotação que permite que a classe intercepte a exception que acontecer na camada de resource
@@ -20,13 +21,27 @@ public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus statusHttp = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(statusHttp.value());
 		err.setError("Resource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		return ResponseEntity.status(statusHttp).body(err);
+	}
+	
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		HttpStatus statusHttp = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(statusHttp.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(statusHttp).body(err);
 	}
 
 }
--- backend/src/main/java/com/eliezerrb/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/eliezerrb/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.eliezerrb.dscatalog.dto.CategoryDTO;
 import com.eliezerrb.dscatalog.entities.Category;
 import com.eliezerrb.dscatalog.repositories.CategoryRepository;
+import com.eliezerrb.dscatalog.services.exceptions.DatabaseException;
 import com.eliezerrb.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -71,6 +74,22 @@ public class CategoryService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
 		}
+	}
+	
+
+	// Não colocado o @Transactional no delete para poder capturar a exceptiondo BD
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 	
 	
