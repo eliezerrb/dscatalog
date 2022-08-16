35fcff1539a48ecd9ca57a347d54720cc76929f5
eliezerrb <eliezerrezende@outlook.com>
2022-08-16 19:50:11 -0300

    Simulando comportamentos diversos com Mockito que tem retorno - findAll, save, findById

--- backend/src/test/java/com/eliezerrb/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/eliezerrb/dscatalog/services/ProductServiceTests.java
@@ -1,19 +1,27 @@
 package com.eliezerrb.dscatalog.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.eliezerrb.dscatalog.entities.Product;
 import com.eliezerrb.dscatalog.repositories.ProductRepository;
 import com.eliezerrb.dscatalog.services.exceptions.DatabaseException;
 import com.eliezerrb.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.eliezerrb.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -27,14 +35,28 @@ public class ProductServiceTests {
 	private long ExistingId;
 	private long nonExistingId;
 	private long dependentId;
+	private PageImpl<Product> page;
+	private Product product;
 	
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		
 		ExistingId = 1L;
-		nonExistingId = 1000L;
-		dependentId = 4L;
+		nonExistingId = 2L;
+		dependentId = 3L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
+		
+		
+		//Simulando com Mockito metodo que retorna alguma coisa 
+		Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+		
+		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+		
+		Mockito.when(repository.findById(ExistingId)).thenReturn(Optional.of(product));
+		
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
 				
 		// Quando chamado o repository.deleteById Mockado de id existente  o metodo não faz nada
 		// Se apagar o Mockito e fazer o import da certo porque fica estático
