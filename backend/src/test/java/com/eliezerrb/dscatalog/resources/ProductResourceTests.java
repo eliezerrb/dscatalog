45e6d5ee6ff1fa0278af9d93eb50e6ffe32b2948
eliezerrb <eliezerrezende@outlook.com>
2022-08-17 22:13:16 -0300

    Começando testes na camada web(controlador) - ProductResourceTest

---
+++ backend/src/test/java/com/eliezerrb/dscatalog/resources/ProductResourceTests.java
@@ -0,0 +1,54 @@
+package com.eliezerrb.dscatalog.resources;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.eliezerrb.dscatalog.dto.ProductDTO;
+import com.eliezerrb.dscatalog.services.ProductService;
+import com.eliezerrb.dscatalog.tests.Factory;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTests {
+	
+	// MockMvc utilizado para chamar endpoint no teste
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private ProductService service;
+	
+	private ProductDTO productDTO;
+	
+	// PageImpl ao invez de Page porque ele é objeto de página concreto e aceita o new
+	private PageImpl<ProductDTO> page;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		productDTO = Factory.createProductDTO();
+		page = new PageImpl<>(List.of(productDTO));
+		
+		// Simulando mockado service.findAllPaged com qualquer argumento(any)
+		when(service.findAllPaged(any())).thenReturn(page);
+	}
+	
+	@Test
+	public void findAllShouldReturnPage() throws Exception{
+		
+		//Fazendo requisição get e esperando o resultado ok(200)
+		mockMvc.perform(get("/products")).andExpect(status().isOk());
+		
+	}
+}
