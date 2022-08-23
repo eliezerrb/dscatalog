98dae8389519ef05631a6578430681b02b7a1b14
eliezerrb <eliezerrezende@outlook.com>
2022-08-22 21:32:33 -0300

    Teste de integração na camada web - findAll - ProductResourceIT

---
+++ backend/src/test/java/com/eliezerrb/dscatalog/resources/ProductResourceIT.java
@@ -0,0 +1,54 @@
+package com.eliezerrb.dscatalog.resources;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIT {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	private Long ExistingId;
+	private Long nonExistingId;
+	private Long countTotalProduct;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		ExistingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProduct = 25L;
+	}
+	
+	@Test
+	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception{
+		
+		ResultActions result = mockMvc.perform(get("/products?page=0&size=12&sort=name,asc")
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.totalElements").value(countTotalProduct));
+		result.andExpect(jsonPath("$.content").exists());
+		
+		// Se o content existe verifico na posição 0, no campo name
+		result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+		result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+		result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+		
+	}
+
+}
--- backend/src/test/java/com/eliezerrb/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/eliezerrb/dscatalog/services/ProductServiceIT.java
@@ -1,15 +1,25 @@
 package com.eliezerrb.dscatalog.services;
 
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import com.eliezerrb.dscatalog.dto.ProductDTO;
 import com.eliezerrb.dscatalog.repositories.ProductRepository;
 import com.eliezerrb.dscatalog.services.exceptions.ResourceNotFoundException;
 
+// @Transactional - Para fazer roolback a cada teste
+// @SpringBootTest - Para carregar o contexto e fazer o teste de integração
+
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 	
 	@Autowired
@@ -44,6 +54,46 @@ public class ProductServiceIT {
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.delete(nonExistingId);
 		});
+		
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPageWhenPage0Size10() {
+		
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(countTotalProduct, result.getTotalElements());
+		
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+		
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertTrue(result.isEmpty());
+		
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+		
+		// Instanciando PageRequest que é a implementação do Page passando 0(página atual), 10(qtde de página), Sort.by("name")(Ordenação por nome)
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
 		
 	}
 }
