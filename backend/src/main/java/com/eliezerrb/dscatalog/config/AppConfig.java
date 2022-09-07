7027a55a2e7bdfacbb7d73775ab1e0e09f2a7540
eliezerrb <eliezerrezende@outlook.com>
2022-09-07 12:29:10 -0300

    Finalizando implementação OAuth2 authorization server - implementando checklist do Spring Cloud OAuth2(AuthorizationServerConfigurerAdapter, ResourceServerConfigurerAdapter, JwtAccessTokenConverter, JwtTokenStore

--- backend/src/main/java/com/eliezerrb/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/eliezerrb/dscatalog/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.eliezerrb.dscatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
@@ -14,5 +16,18 @@ public class AppConfig {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	// Beans objetos que vão ser capazes de acessar o token JWT(ler, decodificar, criar codificando)
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		return tokenConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
 }
