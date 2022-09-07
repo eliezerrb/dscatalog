f9f764f9fbb9849ca7849a2ba5234a8decb8f004
eliezerrb <eliezerrezende@outlook.com>
2022-09-06 21:35:09 -0300

    Implementando checklist do Spring Security(UserDetails, UserDetailsService, WebSecurityConfigurerAdapter, AuthenticationManager)

--- backend/src/main/java/com/eliezerrb/dscatalog/entities/User.java
+++ backend/src/main/java/com/eliezerrb/dscatalog/entities/User.java
@@ -1,9 +1,11 @@
 package com.eliezerrb.dscatalog.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,9 +18,13 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements UserDetails, Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -113,6 +119,45 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		// Implementando o retorno de coleção de perfil
+		
+		/*
+		 	A partir da lista de roles .stream(para começar mecher com lambda) map(transforma cada elemento)
+			Transformar cada elemento do tipo role para um elemento do tipo GrantedAuthority
+			Para cada elemeto do tipo role instanciar um  SimpleGrantedAuthority(classe concreta que implementa a interface GrantedAuthority)
+			role.getAuthority() - precisa para instanciar - passa como parametro que é a string(nome) do perfil
+		*/
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 
