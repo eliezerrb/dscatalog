eb92c632a2e5685f4109b91ec450d88b95c0575c
eliezerrb <eliezerrezende@outlook.com>
2023-02-12 00:24:42 -0300

    Menu hamburguer da Navbar e alterando a versão do node, bootstrap

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,20 +1,39 @@
 import './styles.css';
+import '@popperjs/core';
+import 'bootstrap/js/src/collapse';
 
 const Navbar = () => {
   return (
-    // bg-primary - cor primária do estilo (bootstrap) personalizado
-    // navbar-expand-md - Tamanho médio para frente expanda(bootstrap)
-    // offset-md-2 - espaço, tamanho médio para frente, deixar duas colunas (bootstrap)
-    // collapse navbar-collapse - Menu sumir quando a tela é pequena (bootstrap)
-    <nav className="navbar navbar-expand-md bg-primary main-nav">
+    // bg-primary: cor primária do estilo (bootstrap) personalizado
+    // navbar-expand-md: Tamanho médio para frente expanda(bootstrap)
+    // offset-md-2: espaço, tamanho médio para frente, deixar duas colunas (bootstrap)
+    // collapse navbar-collapse: Menu sumir quando a tela é pequena (bootstrap)
+    // navbar-dark ou navbar-light: especificar se a nav é clara ou escura (bootstrap)
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
       <div className="container-fluid">
         <a href="link" className="nav-logo-text">
           <h4>DS Catalog</h4>
         </a>
-        <div className="collapse navbar-collapse">
+
+        {/* Botão do menu hamburguer */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target=".collapse"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="dscatalog-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link" className="active">HOME</a>
+              <a href="link" className="active">
+                HOME
+              </a>
             </li>
             <li>
               <a href="link">CATÁLOGO</a>
