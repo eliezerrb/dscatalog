ccf38fe494b52b1e4620db5376124b5867fc6706
eliezerrb <eliezerrezende@outlook.com>
2023-02-13 21:21:30 -0300

    Criando componente ProductCard

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from "components/Navbar";
+import ProductCard from "components/ProductCard";
 
 const Catalog = () => {
   return (
@@ -7,9 +8,9 @@ const Catalog = () => {
 
       {/* my-4: Mesma coisa de por o padding na vertical(bootstrap) */}
       {/* container: Delimita uma área na esquerda e direita(bootstrap) */}
-      
+
       <div className="container my-4">
-        <h1>Tela de Catalog</h1>
+        <ProductCard />
       </div>
     </>
   );
