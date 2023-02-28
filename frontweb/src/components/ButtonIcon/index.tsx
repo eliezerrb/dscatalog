a6509917ec3fc0a000e0b6936527f6df686a7432
eliezerrb <eliezerrezende@outlook.com>
2023-02-28 08:17:17 -0300

    Props - primeiro exemplo

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -2,13 +2,18 @@ import './styles.css';
 
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 
-const ButtonIcon = () => {
+
+type Props = {
+  text: string;
+}
+
+const ButtonIcon = ({ text }: Props) => {
   return (
     <>
       {/* btn - efeito de opacidade ao passar o mouse(bootstrap) */}
       <div className="btn-container">
         <button className="btn btn-primary">
-          <h6>Inicie agora sua busca</h6>
+          <h6>{text}</h6>
         </button>
 
         <div className="btn-icon-container">
