1499a73189a0049e14d9cfc52fdb9285dfe8329c
eliezerrb <eliezerrezende@outlook.com>
2023-03-06 08:03:42 -0300

    ProductDetails HTML

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,6 +1,36 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+
 const ProductDetails = () => {
+    // row - bootstrap
+    // col-xl-6 - bootstrap, a partir de 1200px tela se divide em dois(6 metade de 12)
     return (
-        <h1>Página ProductDetails</h1>
+        <div className="product-datails-container">
+            <div className="product-datails-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>VOLTAR</h2>
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do produto" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do produto</h1>
+                            <ProductPrice price={2345.67}/>
+                        </div>
+
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do produto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione, temporibus!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
