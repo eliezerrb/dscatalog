6df0f80f0de64b0a70c68fc0a99ead26d0eee407
eliezerrb <eliezerrezende@outlook.com>
2023-03-07 08:15:16 -0300

    Pagination HTML

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,3 +1,4 @@
+import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
@@ -68,6 +69,10 @@ const Catalog = () => {
               <ProductCard product={product} />
             </Link>
           </div>
+        </div>
+
+        <div className="row">
+          <Pagination />
         </div>
       </div>
     </>
