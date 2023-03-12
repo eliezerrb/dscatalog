a88954de5df64c8b1e0ee79c0b6b4618a38e774a
eliezerrb <eliezerrezende@outlook.com>
2023-03-12 16:06:14 -0300

    Loaders para ProductDetails

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,36 +1,43 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
-import { type } from 'os';
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Product } from 'types/product';
 import { BASE_URL } from 'util/requests';
+import ProductDetailsLoader from './ProductDetailsLoader';
+import ProductInfoLoader from './ProductInfoLoader';
 
 import './styles.css';
 
 type UrlParams = {
   productId: string;
-}
+};
 
 const ProductDetails = () => {
-
   // Capturar parâmetros de URL
   const { productId } = useParams<UrlParams>();
 
+  // Estado para página carregando
+  const [isLoading, setIsLoading] = useState(false);
 
   // product - nome do estado
   // setProduct - função para atualizar o valor do estado
   // Product - tipo do estado
   const [product, setProduct] = useState<Product>();
 
-  // useEffect - () => {} função que você quer executar(executa quando o componente for montado por padrão), depois lista de dependencia(objs para monitorar), pode ser executado novamente quando um dos objs for alterado 
+  // useEffect - () => {} função que você quer executar(executa quando o componente for montado por padrão), depois lista de dependencia(objs para monitorar), pode ser executado novamente quando um dos objs for alterado
   useEffect(() => {
-    axios.get(`${BASE_URL}/products/${productId}`)
-    .then((response) => {
-      // Atribuindo o resultado do response para o Product com a função setProduct
-      setProduct(response.data);
-    });
+    setIsLoading(true);
+    axios
+      .get(`${BASE_URL}/products/${productId}`)
+      .then((response) => {
+        // Atribuindo o resultado do response para o Product com a função setProduct
+        setProduct(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [productId]);
 
   return (
@@ -46,26 +53,34 @@ const ProductDetails = () => {
         </Link>
         <div className="row">
           <div className="col-xl-6">
-            <div className="img-container">
-              <img
-                // ? - só chama o .name se o obj já foi definido
-                src={product?.imgUrl}
-                alt={product?.name}
-              />
-            </div>
-            <div className="name-price-container">
-              <h1>{product?.name}</h1>
-              {/*só renderiza se a váriavel product estiver definida*/}
-              {product && <ProductPrice price={product?.price} />}
-            </div>
+            {isLoading ? (
+              <ProductInfoLoader />
+            ) : (
+              <>
+                <div className="img-container">
+                  <img
+                    // ? - só chama o .name se o obj já foi definido
+                    src={product?.imgUrl}
+                    alt={product?.name}
+                  />
+                </div>
+                <div className="name-price-container">
+                  <h1>{product?.name}</h1>
+                  {/*só renderiza se a váriavel product estiver definida*/}
+                  {product && <ProductPrice price={product?.price} />}
+                </div>
+              </>
+            )}
           </div>
           <div className="col-xl-6">
-            <div className="description-container">
-              <h2>Descrição do produto</h2>
-              <p>
-                {product?.description}
-              </p>
-            </div>
+            {isLoading ? (
+              <ProductDetailsLoader />
+            ) : (
+              <div className="description-container">
+                <h2>Descrição do produto</h2>
+                <p>{product?.description}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
