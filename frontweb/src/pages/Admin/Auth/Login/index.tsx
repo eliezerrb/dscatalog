e7dbacd07dd38354cfac8bb117ed79727f3c0883
eliezerrb <eliezerrezende@outlook.com>
2023-03-28 19:50:05 -0300

    HTML do card de Login

---
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import ButtonIcon from 'components/ButtonIcon';
+
+import './styles.css';
+
+const Login = () => {
+  return (
+    <div className="base-card login-card">
+      <h1>LOGIN</h1>
+      <form>
+        {/*mb-4: Classe bootStrap margin botton 4 espaço abaixo para outro imput*/}
+        <div className="mb-4">
+          <input
+            type="text"
+            className="form-control base-input"
+            placeholder="Email"
+            name="username"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="password"
+            className="form-control base-input "
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="Fazer login" />
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">Não tem Cadastro?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
