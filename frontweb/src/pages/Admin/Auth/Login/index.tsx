3d68fa1934bb819c5e456d3da2585cf9814424c2
eliezerrb <eliezerrezende@outlook.com>
2023-04-04 08:02:16 -0300

    Primeira validação de dados com React Hook Form

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -14,7 +14,7 @@ type FormData = {
 const Login = () => {
   const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
@@ -32,9 +32,7 @@ const Login = () => {
     <div className="base-card login-card">
       <h1>LOGIN</h1>
       {hasError && (
-        <div className="alert alert-danger">
-          Erro ao tentar efetuar o login
-        </div>
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
       )}
       <form onSubmit={handleSubmit(onSubmit)}>
         {/*mb-4: Classe bootStrap margin botton 4 espaço abaixo para outro imput*/}
@@ -42,21 +40,29 @@ const Login = () => {
         <div className="mb-4">
           {/*Controlar pelo react hook "register" form (biblioteca) */}
           <input
-            {...register('username')}
+            {...register('username', {
+              required: 'Campo obrigatório'
+            })}
             type="text"
             className="form-control base-input"
             placeholder="Email"
             name="username"
           />
+          {/*invalid-feedback - Classe do bootstrap */}
+          {/*d-block - Classe do bootstrap para ficar com o display block */}
+          <div className="invalid-feedback d-block">{errors.username?.message}</div>
         </div>
         <div className="mb-2">
           <input
-            {...register('password')}
+            {...register('password', {
+              required: 'Campo obrigatório'
+            })}
             type="password"
             className="form-control base-input "
             placeholder="Password"
             name="password"
           />
+          <div className="invalid-feedback d-block">{errors.password?.message}</div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
