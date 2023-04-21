732b68cdf6851cc9ce0d1fa68353e8c7a5832281
eliezerrb <eliezerrezende@outlook.com>
2023-04-21 15:31:03 -0300

    Permissionamento em nível de rotas

--- frontweb/src/components/PrivateRoute/index.tsx
+++ frontweb/src/components/PrivateRoute/index.tsx
@@ -1,22 +1,32 @@
 import { Redirect, Route } from 'react-router-dom';
-import { isAuthenticated } from 'util/requests';
+import { Role, hasAnyRoles, isAuthenticated } from 'util/requests';
 
 type Props = {
   children: React.ReactNode;
   path: string;
+  roles?: Role[]
 };
 
 // Vou renderizar o Children que você me informar se tiver autenticado
-const PrivateRoute = ({ children, path }: Props) => {
+// Se não informar o roles vai ser igual lista vazia
+const PrivateRoute = ({ children, path, roles = [] }: Props) => {
   return (
     <Route
       path={path}
-      render={({location}) =>
-        isAuthenticated() ? <>{children}</> : <Redirect to={{
-          pathname: "/admin/auth/login",
-          // permitir redirecionar, após o login para a página protegida que foi chamada
-          state: { from:location }
-        }} />
+      render={({ location }) =>
+        !isAuthenticated() ? (
+          <Redirect
+            to={{
+              pathname: '/admin/auth/login',
+              // permitir redirecionar, após o login para a página protegida que foi chamada
+              state: { from: location },
+            }}
+          />
+        ) : !hasAnyRoles(roles) ? (
+          <Redirect to="/admin/products" />
+        ) : (
+          <>{children}</>
+        )
       }
     />
   );
