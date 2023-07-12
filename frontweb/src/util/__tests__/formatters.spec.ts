0c8814c9ce3101e317f5598f2fde0bd0efae9700
eliezerrb <eliezerrezende@outlook.com>
2023-07-12 07:45:12 -0300

    Block describe e suíte de testes

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,13 +1,41 @@
 import { formatPrice } from "util/formatters";
 
-test('formartPrice should format number pt-br when given 10.1', () => {
+describe('formatPrice for positive numbers', () => {
 
-    // ARRANGE
-    const value =10.1;
+    test('formartPrice should format number pt-br when given 10.1', () => {
 
-    // ACT
-    const result = formatPrice(value);
-
-    // ASSERT
-    expect(result).toEqual("10,10");
+        // ARRANGE
+        const value = 10.1;
+    
+        // ACT
+        const result = formatPrice(value);
+    
+        // ASSERT
+        expect(result).toEqual("10,10");
+    });
+    
+    test('formartPrice should format number pt-br when given 0.1', () => {
+      
+        const result = formatPrice(0.1);
+    
+        expect(result).toEqual("0,10");
+    });
 });
+
+describe('formatPrice for non-positive numbers', () => {
+
+    test('formartPrice should format number pt-br when given 0', () => {
+
+        const result = formatPrice(0);
+
+        expect(result).toEqual("0,00");
+    });
+    
+    test('formartPrice should format number pt-br when given -5.1', () => {
+      
+        const result = formatPrice(-5.1);
+    
+        expect(result).toEqual("-5,10");
+    });
+});
+
