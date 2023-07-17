03d8d7053904c33f5c3f3021423e5c159a87e3a2
eliezerrb <eliezerrezende@outlook.com>
2023-07-17 19:48:15 -0300

    Escrevendo outros testes de clique no Pagination

--- frontweb/src/components/Pagination/__tests__/Pagination.spec.tsx
+++ frontweb/src/components/Pagination/__tests__/Pagination.spec.tsx
@@ -67,7 +67,66 @@ describe('Pagination tests', () => {
         //ASSERT
         // Verificando se o evento foi chamado com o parametro 1, pois a bolinha começa no 0 então a pagina 2 é o parametro 1
         expect(OnChange).toHaveBeenCalledWith(1)
+    })
 
+
+    test('previus arrow should call onChange', () => {
+
+        // Arrange
+        const pageCount = 3;
+        const range = 3;
+        const forcePage = 1;
+
+        const OnChange = jest.fn();
+
+        // ACT
+        render(
+            <Pagination
+                pageCount={pageCount}
+                range={range}
+                OnChange={OnChange}
+                forcePage={forcePage}
+            />
+        );
+
+        const arrowPrevius = screen.getByTestId("arrow-previous");
+
+        // Simular o clique no arrowNext que pegamos pelo data-testid
+        userEvent.click(arrowPrevius);
+
+        //ASSERT
+        // Iniciou a paginação com 1 ("forcePage = 1") clicou no link do anterior tem que chamar o onChange com o valor 0
+        expect(OnChange).toHaveBeenCalledWith(0)
+    })
+
+
+
+    test('page link should call onChange', () => {
+
+        // Arrange
+        const pageCount = 3;
+        const range = 3;
+
+        //jest.fn() - cria um obj de mentira para simular um comportamente de evento (uma chamada de função)
+        const OnChange = jest.fn();
+
+        // ACT
+        render(
+            <Pagination
+                pageCount={pageCount}
+                range={range}
+                OnChange={OnChange}
+            />
+        );
+
+        const page2 = screen.getByText("2");
+
+        // Simular o clique na pagina 2
+        userEvent.click(page2);
+
+        //ASSERT
+        // Verificando se o evento foi chamado com o parametro 1, pois a bolinha começa no 0 então a pagina 2 é o parametro 1
+        expect(OnChange).toHaveBeenCalledWith(1)
     })
 
 })
